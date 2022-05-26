73465aa89f86bfebae2cbf8360f8e22ecdf1ed34
Clayton Mmbehi <30318074+mmbehiclayton@users.noreply.github.com>
2022-05-26 08:49:08 +0300

    Fetch profiles

--- nanny/dashboard/urls.py
+++ nanny/dashboard/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("",views.index, name="dashboard-index"),
+    path("",views.index, name="index"),
+    path("dashboard/", views.dashboard, name="dashboard-index")
+
 ]
--- nanny/dashboard/views.py
+++ nanny/dashboard/views.py
@@ -1,7 +1,20 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from users.models import NannyProfile
 
 # Create your views here.
-@login_required(login_url='user_login')
+
 def index(request):
-     return render(request, 'dashboard/index.html')
+     data=User.objects.all()
+     # ndata=NannyProfile.objects.all()
+     context={
+          'data':data,
+          # 'ndata':ndata
+     }
+     return render(request, 'dashboard/index.html', context)
+
+
+@login_required(login_url='user_login')
+def dashboard(request):
+     return render(request, 'dashboard/dashboard.html')
